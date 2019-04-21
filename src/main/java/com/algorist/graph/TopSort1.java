5c6d6c392516ba85efebc61ce0f32f2ed10e14c6
Chen Song <csong2022@berkeley.edu>
2019-04-21 14:55:22 -0400

    Add topsort1 and test case.

---
+++ src/main/java/com/algorist/graph/TopSort1.java
@@ -0,0 +1,73 @@
+/*
+Copyright 2003 by Steven S. Skiena; all rights reserved.
+
+Permission is granted for use in non-commerical applications
+provided this copyright notice remains intact and unchanged.
+
+This program appears in my book:
+
+"Programming Challenges: The Programming Contest Training Manual"
+by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+See our website www.programming-challenges.com for additional information.
+
+This book can be ordered from Amazon.com at
+
+http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
+package com.algorist.graph;
+
+import com.algorist.datastructure.Stack;
+
+/**
+ * Topologically sort a directed acyclic graph by DFS numbering (DAG)
+ *
+ * @param <T> edge node type.
+ * @author csong2022
+ */
+public class TopSort1<T extends EdgeNode<T>> {
+
+    public TopSort1(Graph<T> g) {
+        DFS<T> dfs = new DFS<>(g);
+        TopSort1Callback<T> callback = new TopSort1Callback<>(dfs);
+
+        for (int i = 1; i <= g.nvertices(); i++)
+            if (!dfs.discovered(i))
+                dfs.search(i, callback);
+
+        Stack<Integer> sorted = callback.sorted();
+        sorted.print();  /* report topological order */
+    }
+
+    private static class TopSort1Callback<T extends EdgeNode<T>> implements GraphSearchCallback<T> {
+        private DFS<T> dfs;
+        private Stack<Integer> sorted;
+
+        public TopSort1Callback(DFS<T> dfs) {
+            this.dfs = dfs;
+            this.sorted = new Stack<>();
+        }
+
+        @Override
+        public void processVertexEarly(int v) {
+        }
+
+        @Override
+        public void processVertexLate(int v) {
+            sorted.push(v);
+        }
+
+        @Override
+        public void processEdge(int x, int y) {
+            DFS.EdgeType edgeType = dfs.edgeClassification(x, y);
+
+            if (edgeType == DFS.EdgeType.BACK)
+                System.out.printf("Warning: directed cycle found, not a DAG\n");
+        }
+
+        public Stack<Integer> sorted() {
+            return this.sorted;
+        }
+    }
+}
---
+++ src/test/java/com/algorist/graph/TopSort1Test.java
@@ -0,0 +1,24 @@
+package com.algorist.graph;
+
+import com.algorist.test.TestCaseWithInput;
+import com.algorist.test.TestEngine;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TopSort1Test implements TestCaseWithInput {
+    @Override
+    public void process(Scanner scanner) {
+        UnweightedGraphReader reader = new UnweightedGraphReader();
+        Graph<UnweightedEdgeNode> g = reader.readGraph(scanner, true);
+        g.print();
+
+        new TopSort1<>(g);
+    }
+
+    @Test
+    public void test() throws IOException {
+        TestEngine.execute("grid", "grid-topsort1-out", this);
+    }
+}
