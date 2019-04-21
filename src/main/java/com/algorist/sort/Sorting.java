dff54050f8d0730d868d6d9e20a8c92c52f4c01f
Chen Song <csong2022@berkeley.edu>
2019-04-21 09:28:35 -0400

    Code refactoring.

--- src/main/java/com/algorist/sort/Sorting.java
+++ src/main/java/com/algorist/sort/Sorting.java
@@ -1,3 +1,21 @@
+/*
+Copyright 2003 by Steven S. Skiena; all rights reserved.
+
+Permission is granted for use in non-commerical applications
+provided this copyright notice remains intact and unchanged.
+
+This program appears in my book:
+
+"Programming Challenges: The Programming Contest Training Manual"
+by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+See our website www.programming-challenges.com for additional information.
+
+This book can be ordered from Amazon.com at
+
+http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
 package com.algorist.sort;
 
 import com.algorist.datastructure.PriorityQueue;
@@ -10,8 +28,14 @@ import static java.util.Objects.compare;
 
 /**
  * Sorting algorithms.
+ *
+ * @author csong2022
  */
 public class Sorting {
+
+    public static <T extends Comparable<T>> void insertionSort(T s[], int l, int h) {
+        insertionSort(s, l, h, Comparator.naturalOrder());
+    }
 
     /**
      * Insertion sort.
@@ -30,8 +54,8 @@ public class Sorting {
         }
     }
 
-    public static <T extends Comparable<T>> void insertionSort(T s[], int l, int h) {
-        insertionSort(s, l, h, Comparator.naturalOrder());
+    public static <T extends Comparable<T>> void selectionSort(T s[], int l, int h) {
+        selectionSort(s, l, h, Comparator.naturalOrder());
     }
 
     /**
@@ -55,8 +79,8 @@ public class Sorting {
         }
     }
 
-    public static <T extends Comparable<T>> void selectionSort(T s[], int l, int h) {
-        selectionSort(s, l, h, Comparator.naturalOrder());
+    public static <T extends Comparable<T>> void quickSort(T s[], int l, int h) {
+        quickSort(s, l, h, Comparator.naturalOrder());
     }
 
     /**
@@ -89,8 +113,8 @@ public class Sorting {
         return firsthigh;
     }
 
-    public static <T extends Comparable<T>> void quickSort(T s[], int l, int h) {
-        quickSort(s, l, h, Comparator.naturalOrder());
+    public static <T extends Comparable<T>> void heapSort(T s[], int l, int h) {
+        heapSort(s, l, h, Comparator.naturalOrder());
     }
 
     /**
@@ -114,8 +138,8 @@ public class Sorting {
         }
     }
 
-    public static <T extends Comparable<T>> void heapSort(T s[], int l, int h) {
-        heapSort(s, l, h, Comparator.naturalOrder());
+    public static <T extends Comparable<T>> int binarySearch(T[] s, T key, int low, int high) {
+        return binarySearch(s, key, low, high, Comparator.naturalOrder());
     }
 
     /**
@@ -145,8 +169,8 @@ public class Sorting {
             return binarySearch(s, key, middle + 1, high, c);
     }
 
-    public static <T extends Comparable<T>> int binarySearch(T[] s, T key, int low, int high) {
-        return binarySearch(s, key, low, high, Comparator.naturalOrder());
+    public static <T extends Comparable<T>> void mergeSort(T s[], int l, int h) {
+        mergeSort(s, l, h, Comparator.naturalOrder());
     }
 
     /**
@@ -187,10 +211,6 @@ public class Sorting {
                 s[k] = aux[j++];
             }
         }
-    }
-
-    public static <T extends Comparable<T>> void mergeSort(T s[], int l, int h) {
-        mergeSort(s, l, h, Comparator.naturalOrder());
     }
 
     private static <T> void swap(T[] s, int i, int j) {
