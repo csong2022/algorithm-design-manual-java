8c28c3b43769064d0bf9dafb26bfcd6339148ce9
Chen Song <csong2022@berkeley.edu>
2019-04-22 11:50:53 -0400

    Add geometry and test cases.

---
+++ src/main/java/com/algorist/geometry/Geometry.java
@@ -0,0 +1,240 @@
+/*
+Copyright 2003 by Steven S. Skiena; all rights reserved.
+
+Permission is granted for use in non-commerical applications
+provided this copyright notice remains intact and unchanged.
+
+This program appears in my book:
+
+"Programming Challenges: The Programming Contest Training Manual"
+by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+See our website www.programming-challenges.com for additional information.
+
+This book can be ordered from Amazon.com at
+
+http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
+package com.algorist.geometry;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+/**
+ * Basic geometric primitives and data types -- Lines, Circles, Segments
+ *
+ * @author csong2022
+ */
+public class Geometry {
+    static final double PI = 3.1415926;        /* ratio of circumference to diameter */
+    static final double EPSILON = 0.000001;    /* a quantity small enough to be zero */
+    static final int DIMENSION = 2;           /* dimension of points */
+
+    static Point readPoint(Scanner scanner) {
+        return new Point(scanner.nextDouble(), scanner.nextDouble());
+    }
+
+    static Line pointsToLine(Point p1, Point p2) {
+        double a, b, c;
+        if (p1.x == p2.x) {
+            a = 1;
+            b = 0;
+            c = -p1.x;
+        } else {
+            b = 1;
+            a = -(p1.y - p2.y) / (p1.x - p2.x);
+            c = -(a * p1.x) - (b * p1.y);
+        }
+
+        return new Line(a, b, c);
+    }
+
+    static Line pointAndSlopeToLine(Point p, double m) {
+        double a = -m;
+        double b = 1;
+        double c = -(a * p.x + b * p.y);
+
+        return new Line(a, b, c);
+    }
+
+    static boolean parallelQ(Line l1, Line l2) {
+        return abs(l1.a - l2.a) <= EPSILON &&
+                abs(l1.b - l2.b) <= EPSILON;
+    }
+
+    static boolean sameLineQ(Line l1, Line l2) {
+        return parallelQ(l1, l2) && abs(l1.c - l2.c) <= EPSILON;
+    }
+
+    static Point intersectionPoint(Line l1, Line l2) {
+        if (sameLineQ(l1, l2)) {
+            System.out.println("Warning: Identical lines, all points intersect.");
+            return new Point(0.0, 0.0);
+        }
+
+        if (parallelQ(l1, l2)) {
+            System.out.println("Error: Distinct parallel lines do not intersect.");
+            return null;
+        }
+
+        double x, y;
+        x = (l2.b * l1.c - l1.b * l2.c) / (l2.a * l1.b - l1.a * l2.b);
+
+        if (abs(l1.b) > EPSILON)    /* test for vertical line */
+            y = -(l1.a * x + l1.c) / l1.b;
+        else
+            y = -(l2.a * x + l2.c) / l2.b;
+
+        return new Point(x, y);
+    }
+
+    static Point closestPoint(Point pIn, Line l) {
+        if (abs(l.b) <= EPSILON) {    /* vertical line */
+            return new Point(-l.c, pIn.y);
+        }
+
+        if (abs(l.a) <= EPSILON) {    /* horizontal line */
+            return new Point(pIn.x, -l.c);
+        }
+
+        Line perp;        /* perpendicular to l through (x,y) */
+        perp = pointAndSlopeToLine(pIn, 1 / l.a); /* non-degenerate line */
+        return intersectionPoint(l, perp);
+    }
+
+    static double distance(Point a, Point b) {
+        double d = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+        return sqrt(d);
+    }
+
+    static boolean pointInBox(Point p, Point b1, Point b2) {
+        return ((p.x >= min(b1.x, b2.x)) && (p.x <= max(b1.x, b2.x))
+                && (p.y >= min(b1.y, b2.y)) && (p.y <= max(b1.y, b2.y)));
+    }
+
+    static boolean segmentsIntersect(Segment s1, Segment s2) {
+        Line l1, l2;        /* lines containing the input segments */
+        Point p;        /* intersection point */
+
+        l1 = pointsToLine(s1.p1, s1.p2);
+        l2 = pointsToLine(s2.p1, s2.p2);
+
+        if (sameLineQ(l1, l2))    /* overlapping or disjoint segments */
+            return (pointInBox(s1.p1, s2.p1, s2.p2) ||
+                    pointInBox(s1.p2, s2.p1, s2.p2) ||
+                    pointInBox(s2.p1, s1.p1, s1.p2) ||
+                    pointInBox(s2.p2, s1.p1, s1.p2));
+
+        if (parallelQ(l1, l2)) return false;
+
+        p = intersectionPoint(l1, l2);
+
+        return pointInBox(p, s1.p1, s1.p2) && pointInBox(p, s2.p1, s2.p2);
+    }
+
+    static double signedTriangleArea(Point a, Point b, Point c) {
+        return ((a.x * b.y - a.y * b.x + a.y * c.x
+                - a.x * c.y + b.x * c.y - c.x * b.y) / 2.0);
+    }
+
+    static double triangleArea(Point a, Point b, Point c) {
+        return abs(signedTriangleArea(a, b, c));
+    }
+
+    static boolean ccw(Point a, Point b, Point c) {
+        return signedTriangleArea(a, b, c) > EPSILON;
+    }
+
+    static boolean cw(Point a, Point b, Point c) {
+        return signedTriangleArea(a, b, c) < -EPSILON;
+    }
+
+    static boolean collinear(Point a, Point b, Point c) {
+        return (abs(signedTriangleArea(a, b, c)) <= EPSILON);
+    }
+
+    static class Line {
+        final double a;        /* x-coefficient */
+        final double b;        /* y-coefficient */
+        final double c;        /* constant term */
+
+        public Line(double a, double b, double c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void print() {
+            System.out.printf("(a=%7.3f,b=%7.3f,c=%7.3f)\n", this.a, this.b, this.c);
+        }
+    }
+
+    static class Point {
+        final double x;
+        final double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(Point other) {
+            this(other.x, other.y);
+        }
+
+        static void print(Point p[], int n) {
+            for (int i = 0; i < n; i++)
+                System.out.printf("(%f,%f)\n", p[i].x, p[i].y);
+        }
+
+        void print() {
+            System.out.printf("%7.3f %7.3f\n", this.x, this.y);
+        }
+    }
+
+    static class Polygon {
+        final Point[] p;       /* array of points in polygon */
+
+        public Polygon(Point[] p) {
+            this.p = p;
+        }
+
+        Point[] toPints() {
+            return Arrays.copyOf(p, p.length);
+        }
+
+        void print() {
+            for (int i = 0; i < p.length; i++)
+                System.out.printf("(%f,%f)\n", p[i].x, p[i].y);
+        }
+    }
+
+    static class Segment {
+        final Point p1, p2;    /* endpoints of line segment */
+
+        public Segment(Point p1, Point p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        void print() {
+            System.out.print("segment: ");
+            this.p1.print();
+            this.p2.print();
+        }
+    }
+
+    static class Circle {
+        final Point c;        /* center of circle */
+        final double r;        /* radius of circle */
+
+        public Circle(Point c, double r) {
+            this.c = c;
+            this.r = r;
+        }
+    }
+
+}
---
+++ src/test/java/com/algorist/geometry/CgTest.java
@@ -0,0 +1,75 @@
+/*
+Copyright 2003 by Steven S. Skiena; all rights reserved.
+
+Permission is granted for use in non-commerical applications
+provided this copyright notice remains intact and unchanged.
+
+This program appears in my book:
+
+"Programming Challenges: The Programming Contest Training Manual"
+by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+See our website www.programming-challenges.com for additional information.
+
+This book can be ordered from Amazon.com at
+
+http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
+package com.algorist.geometry;
+
+import com.algorist.test.TestCaseWithInput;
+import com.algorist.test.TestEngine;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static com.algorist.geometry.Geometry.*;
+
+/**
+ * Driver program for computational geometry routines; give the
+ * basic geometric primitives a workout.
+ *
+ * @author csong2022
+ */
+public class CgTest implements TestCaseWithInput {
+
+    public void process(Scanner scanner) {
+        Geometry.Point p1, p2, q1, q2, i = new Point(0, 0);
+        Geometry.Line l1, l2;
+        Geometry.Segment s1, s2, s3, s4;
+
+        while (scanner.hasNext()) {
+            p1 = readPoint(scanner);
+            p2 = readPoint(scanner);
+            q1 = readPoint(scanner);
+            q2 = readPoint(scanner);
+
+            s1 = new Geometry.Segment(p1, p2);
+            s2 = new Geometry.Segment(q1, q2);
+
+            l1 = pointsToLine(p1, p2);
+            l2 = pointsToLine(q1, q2);
+
+            s1.print();
+            s2.print();
+
+            System.out.println("segments_intersect test");
+            System.out.printf("%d\n", segmentsIntersect(s1, s2) ? 1 : 0);
+
+            System.out.println("intersection point");
+            Point tmp = intersectionPoint(l1, l2);
+            if (tmp != null) {
+                i = tmp;
+            }
+            i.print();
+
+            System.out.println("--------------------------------");
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        TestEngine.execute(this, "cgtest-in", "cgtest-out");
+    }
+}
