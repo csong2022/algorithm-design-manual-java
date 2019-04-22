7a2aa845f5f66b5c501f6e3139a2b736a06fd25f
Chen Song <csong2022@berkeley.edu>
2019-04-22 11:41:26 -0400

    Add elevator and test case.

---
+++ src/main/java/com/algorist/dp/Elevator.java
@@ -0,0 +1,110 @@
+/*
+Copyright 2003 by Steven S. Skiena; all rights reserved.
+
+Permission is granted for use in non-commerical applications
+provided this copyright notice remains intact and unchanged.
+
+This program appears in my book:
+
+"Programming Challenges: The Programming Contest Training Manual"
+by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+See our website www.programming-challenges.com for additional information.
+
+This book can be ordered from Amazon.com at
+
+http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
+package com.algorist.dp;
+
+import static java.lang.Math.min;
+
+/**
+ * Elevator stop optimization via dynamic programming.
+ *
+ * @author csong2022
+ */
+public class Elevator {
+    static final int NFLOORS = 25;    /* the height of the building in floors */
+    static final int MAX_RIDERS = 50; /* what is the capacity of the elevator? */
+    static final int MAXINT = 100007;
+
+    int[] stops;          /* what floor does everyone get off at? */
+    int[][] m;            /* dynamic programming cost table */
+    int[][] p;            /* dynamic programming parent table */
+    private int nriders;  /* number of riders */
+    private int nstops;   /* number of allowable stops */
+
+    public Elevator(int[] stops, int nstops) {
+        this.nriders = stops.length - 1;
+        this.nstops = nstops;
+        this.stops = stops;
+        this.m = new int[NFLOORS + 1][nstops + 1];
+        this.p = new int[NFLOORS + 1][nstops + 1];
+    }
+
+    /**
+     * m[i][j] denotes the cost of serving all the riders using j stops,
+     * the last of which is at floor i.  Zero is the originating floor.
+     */
+    int optimizeFloors() {
+        for (int i = 0; i <= NFLOORS; i++) {
+            m[i][0] = floorsWalked(0, MAXINT);
+            p[i][0] = -1;
+        }
+
+        for (int j = 1; j <= nstops; j++)
+            for (int i = 0; i <= NFLOORS; i++) {
+                m[i][j] = MAXINT;
+                for (int k = 0; k <= i; k++) {
+                    int cost = m[k][j - 1] - floorsWalked(k, MAXINT) +
+                            floorsWalked(k, i) + floorsWalked(i, MAXINT);
+                    if (cost < m[i][j]) {
+                        m[i][j] = cost;
+                        p[i][j] = k;
+                    }
+                }
+            }
+
+        int laststop = 0;    /* the elevator's last stop */
+        for (int i = 1; i <= NFLOORS; i++)
+            if (m[i][nstops] < m[laststop][nstops])
+                laststop = i;
+
+        return laststop;
+    }
+
+    int floorsWalked(int previous, int current) {
+        int nsteps = 0;        /* total distance traveled */
+
+        for (int i = 1; i <= nriders; i++)
+            if (stops[i] > previous && stops[i] <= current)
+                nsteps += min(stops[i] - previous, current - stops[i]);
+
+        return nsteps;
+    }
+
+    void reconstructPath(int lastfloor, int stopsToGo) {
+        if (stopsToGo > 1)
+            reconstructPath(p[lastfloor][stopsToGo], stopsToGo - 1);
+
+        System.out.printf("%d\n", lastfloor);
+    }
+
+    void printCostTable() {
+        printMatrix(m);
+    }
+
+    void printParentTable() {
+        printMatrix(p);
+    }
+
+    private void printMatrix(int[][] m) {
+        for (int j = 0; j <= nstops; j++) {
+            for (int i = 0; i <= NFLOORS; i++)
+                System.out.printf("%3d", m[i][j]);
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/com/algorist/dp/ElevatorTest.java
@@ -0,0 +1,40 @@
+package com.algorist.dp;
+
+import com.algorist.test.TestCaseWithInput;
+import com.algorist.test.TestEngine;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static com.algorist.dp.Elevator.MAX_RIDERS;
+
+public class ElevatorTest implements TestCaseWithInput {
+    public void process(Scanner scanner) {
+        int nriders = scanner.nextInt();
+        int nstops = scanner.nextInt();
+
+        int[] stops = new int[MAX_RIDERS];
+        for (int i = 1; i <= nriders; i++)
+            stops[i] = scanner.nextInt();
+
+        Elevator elevator = new Elevator(stops, nstops);
+
+        for (int i = 1; i <= nriders; i++)
+            System.out.printf("%d\n", elevator.stops[i]);
+
+        int laststop = elevator.optimizeFloors();
+
+        elevator.printCostTable();
+        System.out.println();
+        elevator.printParentTable();
+
+        System.out.printf("cost = %d\n", elevator.m[laststop][nstops]);
+
+        elevator.reconstructPath(laststop, nstops);
+    }
+
+    @Test
+    public void test() throws Exception {
+        TestEngine.execute(this, "elevator-in", "elevator-out");
+    }
+}
