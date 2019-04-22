654e4c22d6a6887b7976925ee53677d4b8e592ed
Chen Song <csong2022@berkeley.edu>
2019-04-21 23:01:59 -0400

    Add fib and test case.

--- src/main/java/com/algorist/backtrack/Sudoku.java
+++ src/main/java/com/algorist/backtrack/Sudoku.java
@@ -1,3 +1,21 @@
+/*
+Copyright 2003 by Steven S. Skiena; all rights reserved.
+
+Permission is granted for use in non-commerical applications
+provided this copyright notice remains intact and unchanged.
+
+This program appears in my book:
+
+"Programming Challenges: The Programming Contest Training Manual"
+by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+See our website www.programming-challenges.com for additional information.
+
+This book can be ordered from Amazon.com at
+
+http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
 package com.algorist.backtrack;
 
 import java.io.IOException;
@@ -10,7 +28,9 @@ import static com.algorist.backtrack.Sudoku.Board.DIMENSION;
 import static com.algorist.backtrack.Sudoku.Board.copy;
 
 /**
- * A backtracking program to solve Seduku
+ * A backtracking program to solve Sudoku.
+ *
+ * @author csong2022
  */
 public class Sudoku implements BacktrackCallback<Sudoku.Board> {
 
